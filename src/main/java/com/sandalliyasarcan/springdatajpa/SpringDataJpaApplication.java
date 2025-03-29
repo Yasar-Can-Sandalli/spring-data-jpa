63e6189564a38fd8bd60b16785000ad9f7ebfa6c
Yasar Can Sandalli <sandalliyasarcan@gmail.com>
2025-03-29 15:16:52 +0300

    Controller,Servic ve Repository katmanları oluşturuldu.Hibernate anotasyonları kullanarak bir postgre veritabanına bağlanılıp veri tabanına veri ekleyen bir put isteği oluşturuldu ve bu isteği karşılayan fonksiyonlar tanımlandı.

--- src/main/java/com/sandalliyasarcan/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/sandalliyasarcan/springdatajpa/SpringDataJpaApplication.java
@@ -1,11 +1,14 @@
 package com.sandalliyasarcan.springdatajpa;
 
-import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = "com.sandalliyasarcan")
+@ComponentScan(basePackages = "com.sandalliyasarcan")
+@EnableJpaRepositories(basePackages = "com.sandalliyasarcan") // JPA repositorisini aktif etme !
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
